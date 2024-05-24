d54bcd046eace83757f8821d0a2ddfd061d40efb
ChrisKnowsDev <chrisharley81@gmail.com>
2024-05-24 14:15:31 -0400

    created initial top-menu component for landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import TopMenu from "@/components/shared/top-menu";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-      <h1 className="text-4xl">start here</h1>
+      {/* nav */}
+      <TopMenu/>
+      {/* image */}
+      {/* info */}
     </main>
   );
 }
---
+++ components/shared/top-menu.tsx
@@ -0,0 +1,9 @@
+export default function TopMenu() {
+  return (
+    <header className="w-full">
+      <nav className="mx-4 mt-8">
+        <h3 className="text-4xl font-bold">trackIT</h3>
+      </nav>
+    </header>
+  )
+}
