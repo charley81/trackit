4d87e0fff94a81551e85094387ed83bb63ecd60a
ChrisKnowsDev <chrisharley81@gmail.com>
2024-05-24 14:55:36 -0400

    landing page complete mobile

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
-import TopMenu from "@/components/shared/top-menu";
-import Image from "next/image";
+import Details from '@/components/landing/details'
+import TopMenu from '@/components/shared/top-menu'
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main>
-      {/* nav */}
-      <TopMenu/>
-      {/* image */}
-      {/* info */}
+    <main className="px-4">
+      <TopMenu />
+      <div className="relative h-80 mt-20">
+        <Image
+          src="https://utfs.io/f/82fcfc8d-bad0-46fb-9579-3082ed0cfc49-6npzhe.svg"
+          alt=""
+          fill
+          sizes="100vw"
+          className="object-cover rounded"
+          priority
+        />
+      </div>
+      <Details />
     </main>
-  );
+  )
 }
--- components/shared/top-menu.tsx
+++ components/shared/top-menu.tsx
@@ -1,7 +1,7 @@
 export default function TopMenu() {
   return (
     <header className="w-full">
-      <nav className="mx-4 mt-8">
+      <nav className="mt-8">
         <h3 className="text-4xl font-bold">trackIT</h3>
       </nav>
     </header>
